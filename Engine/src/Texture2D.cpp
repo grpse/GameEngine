049355c2ee43b24161cd3e2f0e4cf2b667912238
Gilberto <gilbertoribeiropazdarosa@gmail.com>
2018-01-03 20:07:10 -0200

    [add] texture 2d opengl error handling CHECK_POINT

--- Engine/src/Texture2D.cpp
+++ Engine/src/Texture2D.cpp
@@ -1,8 +1,9 @@
 #include "Texture2D.h"
+#include "GLErrorHandling.h"
 
 Texture2D::Texture2D()
 {
-    glGenTextures(1, &mID);
+    GLCall(glGenTextures(1, &mID));
     setupDefaults();
 }
 
@@ -43,14 +44,14 @@ uint Texture2D::getFormat() const
 
 void Texture2D::setParameter(uint name, uint value)
 {
-    glTexParameteri(GL_TEXTURE_2D, name, value);
+    GLCall(glTexParameteri(GL_TEXTURE_2D, name, value));
 }
 
 void Texture2D::loadData(const void* data, uint width, uint height)
 {
     mWidth = width;
     mHeight = height;
-    glTexImage2D(
+    GLCall(glTexImage2D(
         GL_TEXTURE_2D, 
         0, 
         GL_RGBA, 
@@ -60,23 +61,23 @@ void Texture2D::loadData(const void* data, uint width, uint height)
         mFormat, 
         GL_UNSIGNED_BYTE, 
         data
-    );
+    ));
 }
 
 void Texture2D::generateMipMaps()
 {
-    glGenerateMipmap(GL_TEXTURE_2D);
+    GLCall(glGenerateMipmap(GL_TEXTURE_2D));
 }
 
 void Texture2D::start() 
 {
-    glActiveTexture(GL_TEXTURE0 + mIndex);
-    glBindTexture(GL_TEXTURE_2D, mID);
+    GLCall(glActiveTexture(GL_TEXTURE0 + mIndex));
+    GLCall(glBindTexture(GL_TEXTURE_2D, mID));
 }
 
 void Texture2D::stop()
 {
-    glBindTexture(GL_TEXTURE_2D, 0);
+    GLCall(glBindTexture(GL_TEXTURE_2D, 0));
 }
 
 uint Texture2D::getWidth() const 
