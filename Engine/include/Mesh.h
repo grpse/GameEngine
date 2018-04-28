3cde1b47c961d0132a708ea12ebc3e345e401b34
Gilberto <gilbertoribeiropazdarosa@gmail.com>
2018-04-28 14:57:58 -0300

    [chg] mesh.cpp added

--- Engine/include/Mesh.h
+++ Engine/include/Mesh.h
@@ -32,124 +32,20 @@ struct Vertex {
 class Mesh {
 
 public:
-	Mesh()
-		: mMarkedAsCopy(false)
-    {
-		mIsIndexed = false;
-    }
+	Mesh();
+	Mesh(const Mesh& other);
+	Mesh(VertexArray& vertexArray, IndexBuffer& indexBuffer);
+	Mesh(VertexArray& vertexArray, uint indexStart, uint indexEnd);
+	Mesh(VertexArray& vertexArray, uint indexStart, uint indexEnd, std::vector<Vertex> vertices);
+	~Mesh();
 
-	Mesh(const Mesh& other)
-	{
-		mVertexArray = other.mVertexArray;
-		mIndexBuffer = other.mIndexBuffer;
-		mIsIndexed = other.mIsIndexed;
-		mIndexStart = other.mIndexStart;
-		mIndexEnd = other.mIndexEnd;
-	}
+	void render(const Renderer& renderer) const;
+	void load(VertexArray& vertexArray, IndexBuffer& indexBuffer);
+	const std::vector<Vertex>& getVertices() const;
+	const VertexArray& getVertexArray() const;
+	const IndexBuffer& getIndexBuffer() const;
 
-    Mesh(VertexArray& vertexArray, IndexBuffer& indexBuffer)
-    {
-        load(vertexArray, indexBuffer);
-    }
-
-	Mesh(VertexArray& vertexArray, uint indexStart, uint indexEnd)
-	{
-		mIsIndexed = false;
-		mVertexArray = vertexArray;
-		mIndexStart = indexStart;
-		mIndexEnd = indexEnd;
-	}
-
-	Mesh(VertexArray& vertexArray, uint indexStart, uint indexEnd, std::vector<Vertex> vertices)
-	{
-		mIsIndexed = false;
-		mVertexArray = vertexArray;
-		mIndexStart = indexStart;
-		mIndexEnd = indexEnd;
-		mVertices = vertices;
-	}
-
-    ~Mesh()
-    {
-    }
-
-	void render(const Renderer& renderer) const
-	{
-		if (mIsIndexed)
-		{
-			renderer.render(mVertexArray, mIndexBuffer);
-		}
-		else 
-		{
-			renderer.render(mVertexArray, mIndexStart, mIndexEnd);
-		}
-
-	}
-
-    void load(VertexArray& vertexArray, IndexBuffer& indexBuffer)
-    {
-        mVertexArray = vertexArray;
-        mIndexBuffer = indexBuffer;
-		mIsIndexed = true;
-    }
-
-	const std::vector<Vertex>& getVertices() const
-	{
-		return mVertices;
-	}
-
-    const VertexArray& getVertexArray() const
-    {
-        return mVertexArray;
-    }
-
-    const IndexBuffer& getIndexBuffer() const
-    {
-        return mIndexBuffer;
-    }
-
-	static Mesh createQuad() {
-		VertexArray vao;
-		IndexBuffer ibo;
-
-		Vertex v1, v2, v3, v4;
-
-		v1.position = { -1, 0, -1 };
-		v2.position = { 1, 0, -1 }; 
-		v3.position = { 1, 0,  1 }; 
-		v4.position = { -1, 0,  1 };
-
-		v1.texturecoord0 = { 0, 0 };
-		v2.texturecoord0 = { 1, 0 };
-		v3.texturecoord0 = { 1, 1 };
-		v4.texturecoord0 = { 0, 1 };
-
-		v1.normal = { 0, 0, -1 };
-		v2.normal = { 0, 0, -1 };
-		v3.normal = { 0, 0, -1 };
-		v4.normal = { 0, 0, -1 };
-
-		Vertex description[] = {
-			v1, v2, v3, v4
-		};
-
-		uint indices[] = {
-			0, 1, 2, 0, 2, 3
-		};
-
-		vao.createVertexBuffer<Vertex>(description, 4, {
-			{3, POSITION, false},
-			{3, NORMAL, true},
-			{2, TEXCOORD0, false}
-		});
-		
-		ibo.load<uint>(indices, 6);
-
-		return Mesh(vao, ibo);
-	}
-
-	uint mIndexStart;
-	uint mIndexEnd;
+	static Mesh createQuad();
 
 private:
     IndexBuffer mIndexBuffer;
@@ -157,6 +53,9 @@ private:
 	bool mIsIndexed;
 	bool mMarkedAsCopy;
 	std::vector<Vertex> mVertices;
+
+	uint mIndexStart;
+	uint mIndexEnd;
 
 	friend class Renderer;
 };
